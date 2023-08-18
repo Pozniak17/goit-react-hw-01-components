466476cff3c38a0dd812b86410d6f983712f8fdd
Yevhenii Pozniak <jekamanu@gmail.com>
2023-08-18 11:43:35 +0300

    update props

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -3,7 +3,7 @@ import { Item, Status, Avatar, Name } from './FriendListItem.styled';
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <Item>
-      <Status online={isOnline}></Status>
+      <Status status={isOnline}></Status>
       <Avatar src={avatar} alt="User avatar" width="54" />
       <Name>{name}</Name>
     </Item>
