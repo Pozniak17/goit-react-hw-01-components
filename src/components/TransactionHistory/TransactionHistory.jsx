a2b3c48b3903d376407bb8fc4f24b9793f81fee6
Yevhenii Pozniak <jekamanu@gmail.com>
2023-08-18 14:46:47 +0300

    add PropTypes to TransactionHistory

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 import {
   Container,
   TransactionHead,
@@ -31,25 +33,13 @@ export const TransactionHistory = ({ items }) => {
   );
 };
 
-// <table class="transaction-history">
-//   <thead>
-//     <tr>
-//       <th>Type</th>
-//       <th>Amount</th>
-//       <th>Currency</th>
-//     </tr>
-//   </thead>
-
-//   <tbody>
-//     <tr>
-//       <td>Invoice</td>
-//       <td>125</td>
-//       <td>USD</td>
-//     </tr>
-//     <tr>
-//       <td>Withdrawal</td>
-//       <td>85</td>
-//       <td>USD</td>
-//     </tr>
-//   </tbody>
-// </table>;
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
